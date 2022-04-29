9b7ba30d89eb6ef7ec8cabbfc1a4200f41ba00aa
Otávio <sedanootavio@gmail.com>
2022-04-29 15:38:28 -0300

    add BEM and html more semantic

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 const input = document.querySelector('input')
 const ul = document.querySelector('ul')
-const clearButton = document.querySelector('.btn-clear')
-const submitButton = document.querySelector('.btn-submit')
+const clearButton = document.querySelector('.clear-btn')
+const submitButton = document.querySelector('.submit-btn')
 const containerItems = document.querySelector('.container__items')
-const containerBtnClear = document.querySelector('.container__btn-clear')
+const containerClearBtn = document.querySelector('.container__clear-btn')
 const form = document.querySelector('form')
 const containerValidations = document.querySelector('.container__validations')
 
 let editElement
 let editFlag = false
-let editId = ""
+let editId = ''
 
 form.addEventListener('submit', createLi)
 clearButton.addEventListener('click', removeAllItems)
 containerItems.addEventListener('click', checkItem)
 window.addEventListener('DOMContentLoaded', setupItems)
 
-function setReset() {
-  input.value = ""
-  editId = ""
+function setReset () {
+  input.value = ''
+  editId = ''
   editFlag = false
   submitButton.textContent = 'Add'
 }
 
-const firstLetterUppercase = text => 
+const firstLetterUppercase = text =>
   text.charAt().toUpperCase() + text.slice(1)
 
-function repeatedItem(value) {
-  let items = getLocalStorage()
+function repeatedItem (value) {
+  const items = getLocalStorage()
 
   const filteredItems = items.filter((item) => {
     const itemValue = item.value.toLowerCase()
     const inputValue = value.toLowerCase()
-    if (itemValue == inputValue) {
+    if (itemValue === inputValue) {
       return itemValue
     }
   })
@@ -40,7 +43,7 @@ function repeatedItem(value) {
   return filteredItems
 }
 
-function createAlert(text, action) {
+function createAlert (text, action) {
   const containerAlert = document.createElement('div')
   const errorAction = action === 'error'
   if (containerValidations.children.length < 5 && errorAction) {
@@ -51,106 +54,105 @@ function createAlert(text, action) {
     containerAlert.innerHTML = `<span class='alert-${action}'>${text}</span>`
   }
   containerValidations.append(containerAlert)
-  let messages = []
+  const messages = []
   messages.push(containerAlert)
   removeValidation(messages)
 }
 
-function removeValidation(message) {
+function removeValidation (message) {
   setTimeout(() => {
-   const firstMessage = message.shift()
-   firstMessage.remove()
+    const firstMessage = message.shift()
+    firstMessage.remove()
   }, 2500)
 }
 
-function createContainerItem(value, containerItem) {
+function createContainerItem (value, containerItem) {
   containerItem.classList.add('container__item')
   containerItem.innerHTML = `
-  <li>${firstLetterUppercase(value)}</li> 
+  <li class="item">${firstLetterUppercase(value)}</li> 
   <div class="container__btns"> 
-    <button onclick="getItemToEdit(event)" type="button" class="btn-edit">
+    <button onclick="getItemToEdit(event)" type="button" class="edit-btn">
       <i class="fa-solid fa-pen-to-square"></i>
     </button>
-    <button onclick="removeItem(event)" type="button" class="btn-remove" >
+    <button onclick="removeItem(event)" type="button" class="remove-btn" >
       <i class="fa-solid fa-x"></i>
     </button>
   </div>`
 }
 
-function setAttrContainerItem(id, isChecked, containerItem) {
-  let attr = document.createAttribute("data-id")
-  let check = document.createAttribute("isChecked")
+function setAttrContainerItem (id, isChecked, containerItem) {
+  const attr = document.createAttribute('data-id')
+  const check = document.createAttribute('isChecked')
   check.value = isChecked
   attr.value = id
   containerItem.setAttributeNode(attr)
   containerItem.setAttributeNode(check)
 }
 
-function isPluralItem(value, textPlural, textSingular) {
+function isPluralItem (value, textPlural, textSingular) {
   const endsWithS = input.value.endsWith('s')
   return endsWithS
-  ? `"${firstLetterUppercase(value)}" ${textPlural}`
-  : `"${firstLetterUppercase(value)}" ${textSingular}`
+    ? `"${firstLetterUppercase(value)}" ${textPlural}`
+    : `"${firstLetterUppercase(value)}" ${textSingular}`
 }
 
-function createItem(value) {
+function createItem (value) {
   const id = new Date().getTime().toString()
   const containerItem = document.createElement('div')
-  let isChecked = false
+  const isChecked = false
 
   createContainerItem(value, containerItem)
   setAttrContainerItem(id, isChecked, containerItem)
-  
+
   if (repeatedItem(value).length > 0) {
     createAlert(
       isPluralItem(
-      value, 'Já foram adicionados', 'Já foi adicionado'),
+        value, 'Já foram adicionados', 'Já foi adicionado'),
       'error'
     )
   } else {
     addToLocalStorage(id, value, isChecked)
     ul.appendChild(containerItem)
     createAlert(
-      isPluralItem(value, 'foram adicionados', 'foi adicionado'), 
-      `successe`
+      isPluralItem(value, 'foram adicionados', 'foi adicionado'),
+      'successe'
     )
   }
   setReset()
 }
 
-function getItemToEdit(e) {
+function getItemToEdit (e) {
   editFlag = true
   const liGet = e.currentTarget.parentElement.parentElement
   editElement = e.currentTarget.parentElement.previousElementSibling
 
   submitButton.textContent = 'Edit'
   input.value = editElement.innerHTML
-  editId = liGet.dataset.id;
+  editId = liGet.dataset.id
   input.focus()
 }
 
-function editItem(value) {
-
+function editItem (value) {
   if (repeatedItem(value).length > 0) {
     createAlert(
-      isPluralItem(value, 'Já foram adicionados', 'Já foi adicionado'), 
+      isPluralItem(value, 'Já foram adicionados', 'Já foi adicionado'),
       'error'
     )
   } else {
-    editElement.innerHTML = firstLetterUppercase(value);
+    editElement.innerHTML = firstLetterUppercase(value)
     createAlert('Alterado com sucesso', 'successe')
     editLocalStorage(editId, value)
     setReset()
   }
 }
 
-function createLi(e) {
+function createLi (e) {
   e.preventDefault()
   const value = input.value.trim()
-  
+
   if (value !== '' && !editFlag) {
     containerItems.style.display = 'block'
-    containerBtnClear.style.display = 'flex'
+    containerClearBtn.style.display = 'flex'
     createItem(value)
   } else if (value !== '' && editFlag) {
     editItem(value)
@@ -159,20 +161,20 @@ function createLi(e) {
   }
 }
 
-function removeAllItems() {
+function removeAllItems () {
   if (ul.childElementCount > 0) {
-    while(ul.firstChild) {
+    while (ul.firstChild) {
       ul.removeChild(ul.firstChild)
     }
   }
-  containerBtnClear.style.display = 'none'
+  containerClearBtn.style.display = 'none'
   containerItems.style.display = 'none'
   createAlert('Lista foi limpa', 'successe')
-  localStorage.removeItem("list")
+  localStorage.removeItem('list')
   setReset()
 }
 
-function checkItem(e) {
+function checkItem (e) {
   const isContainerItem = e.target.className === 'container__item'
   const isLi = e.target.localName === 'li'
 
@@ -184,7 +186,7 @@ function checkItem(e) {
   checkFromLocalStorage(e)
 }
 
-function removeItem(e) {
+function removeItem (e) {
   const liGet = e.currentTarget.parentElement.parentElement
   const id = liGet.dataset.id
 
@@ -192,26 +194,26 @@ function removeItem(e) {
   removeFromLocalStorage(id)
   setReset()
 
-  if (ul.childElementCount == 0) {
+  if (ul.childElementCount === 0) {
     containerItems.style.display = 'none'
-    containerBtnClear.style.display = 'none'
+    containerClearBtn.style.display = 'none'
   }
 }
 
-function getLocalStorage() {
-  return localStorage.getItem("list")
-    ? JSON.parse(localStorage.getItem("list"))
+function getLocalStorage () {
+  return localStorage.getItem('list')
+    ? JSON.parse(localStorage.getItem('list'))
     : []
 }
 
-function addToLocalStorage(id, value, isChecked) {
-  const props = { id, value, isChecked}
-  let items = getLocalStorage()
+function addToLocalStorage (id, value, isChecked) {
+  const props = { id, value, isChecked }
+  const items = getLocalStorage()
   items.push(props)
-  localStorage.setItem("list", JSON.stringify(items))
+  localStorage.setItem('list', JSON.stringify(items))
 }
 
-function checkFromLocalStorage(e) {
+function checkFromLocalStorage (e) {
   const containerId = e.srcElement.dataset.id
   const liFromId = e.srcElement.parentElement.dataset.id
   const isLi = e.target.localName === 'li'
@@ -226,11 +228,11 @@ function checkFromLocalStorage(e) {
       }
       return item
     })
-    localStorage.setItem("list", JSON.stringify(items))
+    localStorage.setItem('list', JSON.stringify(items))
   }
 }
 
-function removeFromLocalStorage(id) {
+function removeFromLocalStorage (id) {
   let items = getLocalStorage()
 
   items = items.filter((item) => {
@@ -238,10 +240,10 @@ function removeFromLocalStorage(id) {
       return item
     }
   })
-  localStorage.setItem("list", JSON.stringify(items))
+  localStorage.setItem('list', JSON.stringify(items))
 }
 
-function editLocalStorage(id, value) {
+function editLocalStorage (id, value) {
   let items = getLocalStorage()
 
   items = items.map((item) => {
@@ -250,11 +252,11 @@ function editLocalStorage(id, value) {
     }
     return item
   })
-  localStorage.setItem("list", JSON.stringify(items))
+  localStorage.setItem('list', JSON.stringify(items))
 }
 
-function setupItems() {
-  let items = getLocalStorage()
+function setupItems () {
+  const items = getLocalStorage()
   if (items.length > 0) {
     items.forEach(item => {
       loadListItem(item.id, item.value, item.isChecked)
@@ -262,10 +264,10 @@ function setupItems() {
   }
 }
 
-function loadListItem(id, value, isChecked) {
-  const containerItem = document.createElement("div")
+function loadListItem (id, value, isChecked) {
+  const containerItem = document.createElement('div')
   containerItems.style.display = 'block'
-  containerBtnClear.style.display = 'flex'
+  containerClearBtn.style.display = 'flex'
   createContainerItem(value, containerItem)
   setAttrContainerItem(id, isChecked, containerItem)
   if (isChecked) {
